9d721f64511856fef51f9205ecb2e503c6052b61
brgermano <44592272+brgermano@users.noreply.github.com>
2019-10-01 09:31:16 -0300

    Just testing the github codereview
    Testing :)

--- src/sagas.js
+++ src/sagas.js
@@ -2,6 +2,8 @@ import 'regenerator-runtime/runtime';
 import { fork } from 'redux-saga/effects';
 import ListSaga from './components/List/sagas';
 
+console.log(ListSaga);
+
 function* rootSaga() {
   yield fork(ListSaga);
 }
